0f0aaec4b619aa5d31ed0f935a5970125991b2e2
AnhVu910 <conanshinichi2000@gmail.com>
2022-11-08 13:05:54 +0700

    fix no response data

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
-import initWebRoutes from './route/web';
+import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
-require('dotenv').config();
+import cors from "cors";
+require("dotenv").config();
 
 let app = express();
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 //config app
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
@@ -19,8 +26,8 @@ let port = process.env.PORT || 6969;
 //Port === undefined => port = 6969
 
 app.listen(port, () => {
-    //callback
-    console.log("Backend Nodejs is runing on the port : " + port)
-})
+  //callback
+  console.log("Backend Nodejs is runing on the port : " + port);
+});
 
 connectDB();
